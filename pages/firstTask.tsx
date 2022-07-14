fd020fd008edfe1284b870e13f302c21650be99a
Igor <48488039+hjphopchop@users.noreply.github.com>
2022-07-14 18:22:40 +0300

    feat(wix): upgrate first task

--- pages/firstTask.tsx
+++ pages/firstTask.tsx
@@ -1,8 +1,8 @@
-import React, { useRef } from 'react'
-import Layout from '../components/Layout'
-import { Bar, } from "react-chartjs-2";
+import React from "react";
+import Layout from "../components/Layout";
+import { Bar } from "react-chartjs-2";
 import {
-      Chart as ChartJS,
+  Chart as ChartJS,
   CategoryScale,
   LinearScale,
   PointElement,
@@ -13,8 +13,8 @@ import {
   Legend,
 } from "chart.js";
 
-import { ArcElement, Chart } from 'chart.js';
-import { GenerateDate } from '../interfaces';
+import { ArcElement, Chart } from "chart.js";
+import { GenerateDate } from "../interfaces";
 
 ChartJS.register(
   CategoryScale,
@@ -27,48 +27,46 @@ ChartJS.register(
   Legend
 );
 
- const data =  {
-  
-     labels: ["0-2", "2-4", "4-6", "6-8", "8-10"],
-     datasets: [
-       {
-         label: "IGOR",
-         data: [12, 19, 3, 5, 2, 3],
-       },
-     ],
-   
- };
-
- const options = {
-    plugins: {
-        legend: {
-            display: true,
-        },
+const data = {
+  labels: ["0-2", "2-4", "4-6", "6-8", "8-10"],
+  datasets: [
+    {
+      label: "IGOR",
+      data: [12, 19, 3, 5, 2, 3],
     },
-    elements: {
-        line: {
-            tension: 0,
-            borderWidth: 2,
-            borderColor: "rgba(47,97,68,1)",
-            fill: "start",
-            backgroundColor: "rgba(47,47,68,0.3)",
-        },
-        point: {
-            radius: 0,
-            hitRadius: 0,
-        },
-    },
-    scales: {
-        xAxis: {
-            display: true,
-        },
-        yAxis: {
-            display: false
-        }
-    }
- }
+  ],
+};
 
-export default function FirstTask({ answers }: any) {
+const options = {
+  plugins: {
+    legend: {
+      display: true,
+    },
+  },
+  elements: {
+    line: {
+      tension: 0,
+      borderWidth: 2,
+      borderColor: "rgba(47,97,68,1)",
+      fill: "start",
+      backgroundColor: "rgba(47,47,68,0.3)",
+    },
+    point: {
+      radius: 0,
+      hitRadius: 0,
+    },
+  },
+  scales: {
+    xAxis: {
+      display: true,
+    },
+    yAxis: {
+      display: false,
+    },
+  },
+};
+
+export default function FirstTask({ answers }: GenerateDate) {
   console.log(answers);
 
   return (
@@ -80,10 +78,10 @@ export default function FirstTask({ answers }: any) {
     </Layout>
   );
 }
- export async function getServerSideProps() {
-   const res = await fetch("http://localhost:3000/api/data");
-   const answers = await res.json();
-   return {
-     props: { answers },
-   };
- }
+export async function getServerSideProps() {
+  const res = await fetch("http://localhost:3000/api/data");
+  const answers = await res.json();
+  return {
+    props: { answers },
+  };
+}
